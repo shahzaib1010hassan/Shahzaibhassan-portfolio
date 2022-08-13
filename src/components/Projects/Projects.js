b50f4b3a560dcafe7ab7f200cd9cf3361d02eefb
Shahzaib Hassan <76172514+shahzaib1010hassan@users.noreply.github.com>
2022-08-13 16:49:40 +0500

    initial Done

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import ProjectSummery from "./ProjectSummery";
+import Employers from "./Employers";
+import ProjectList from "./ProjectList";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Container>
+        <ProjectSummery />
+        <Employers />
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
